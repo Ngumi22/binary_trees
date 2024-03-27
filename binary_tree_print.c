27157d8d91a2d6a59a1e99ec67647bd3b5581995
Ngumi22 <mwaurapngumi@gmail.com>
2024-03-27 23:30:29 +0300

    0-binary_tree_node.c

---
+++ binary_tree_print.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_print - Prints a binary tree
+ * @tree: Pointer to the root node of the tree to print
+ */
+void binary_tree_print(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	printf("       .-------(%d)-------.\n", tree->n);
+
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		printf("  .--(%d)--.         .--(%d)--.\n", 
+				tree->left ? tree->left->n : 0,
+				tree->right ? tree->right->n : 0);
+		printf("(%d)     (%d)     (%d)     (%d)\n",
+				tree->left && tree->left->left ? tree->left->left->n : 0,
+				tree->left && tree->left->right ? tree->left->right->n : 0,
+				tree->right && tree->right->left ? tree->right->left->n : 0,
+				tree->right && tree->right->right ? tree->right->right->n : 0);
+	}
+}
